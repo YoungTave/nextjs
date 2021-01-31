4c526cffc123fc7c36bd266bf6ba4b5acee93107
YoungTave <50119889+YoungTave@users.noreply.github.com>
2021-01-31 23:43:05 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Хотите приступить к покупке?</a>
         </h1>
 
         <p className={styles.description}>
